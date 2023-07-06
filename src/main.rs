50a89323b1c794ae641c158f5f42fc659dbe8820
Jordan Whitehead <jordan9001@gmail.com>
2023-07-06 10:24:27 -0600

    switched to a cleaner modal setup

--- src/main.rs
+++ src/main.rs
@@ -15,16 +15,20 @@ use serenity::{
         application::component::ButtonStyle,
         prelude::{prelude::component::ActionRowComponent, ChannelId, UserId, MessageId},
     },
-    collector::ModalInteractionCollectorBuilder,
+    collector::{ModalInteractionCollectorBuilder, ComponentInteractionCollectorBuilder}, builder::CreateComponents,
 };
 
 const ID_VOTE_OPTIONS_INPUT: &str = "InputOptions";
 const ID_VOTE_TYPE: &str = "VoteKind";
 const ID_VOTE_VAL_PREFIX: &str = "VoteVal";
+const ID_VOTE_BTN: &str = "MainVoteBtn";
+const ID_VOTE_LEFT: &str = "VoteLeft";
+const ID_VOTE_RIGHT: &str = "VoteRight";
+const ID_VOTE_SUBMIT: &str = "VoteSubmit";
 
 const DEFAULT_TIMEOUT: Duration = Duration::from_secs(60*60*9);
 
-#[derive(PartialEq, Eq, Debug)]
+#[derive(PartialEq, Eq, Debug, Clone, Copy)]
 struct VoteType(u32);
 const VOTE_APPROVAL: VoteType   = VoteType(1 << 0);
 const VOTE_SCORE: VoteType      = VoteType(1 << 1);
@@ -54,12 +58,30 @@ impl VoteType {
 
 enum CastVotes {
     Select(Vec<usize>), // one or more choices, used for normal or approval voting
-    Score(Vec<(usize, f32)>), // choices associated with a value, also used for rank voting
+    Score(HashMap<usize, f32>), // choices associated with a value, also used for rank voting
+}
+
+impl CastVotes {
+    fn new(vt: VoteType) -> Self {
+        match vt {
+            VOTE_APPROVAL => CastVotes::Select(Vec::new()),
+            VOTE_SCORE => CastVotes::Score(HashMap::new()),
+            VOTE_BORDA => CastVotes::Score(HashMap::new()),
+            _ => panic!("Tried to create CastVotes with unknown vote type"),
+        }
+    }
+}
+
+struct UserVote {
+    votes: CastVotes,
+    fullvote: bool,
+    votemsg: MessageId,
+    page: usize,
 }
 
 struct Vote {
     kind: VoteType,
-    uservotes: HashMap<UserId,(CastVotes, MessageId)>,
+    uservotes: HashMap<UserId,UserVote>,
 }
 
 impl Vote {
@@ -71,60 +93,151 @@ impl Vote {
     }
 }
 
-async fn start_vote(ctx: &Context, cid: ChannelId, votetype: VoteType, vals: Vec<&str>, timeout: Duration) {
-    // send a message (or multiple) to the channel for everyone, with the voting options
-    // depending on the vote, these will be different values to fill in
-    // only 5 rows per message, so multiple messages might be needed
-    // 5 buttons can be per row, but that seems messy
-    // modal shows the thing
-    // and an ephemeral message to display your vote
-    // and you can change your vote as needed?
-    // final submit/view results button will not double submit your vote,
-    // but will just show results in ephemeral again
-
-    let mut i = 0;
-    let num_msg = ((vals.len() -1) / 5) + 1;
-    let vlen = vals.len();
-    while i < vlen {
-        cid.send_message(ctx, |m| {
-            m
-                .content(format!("{} Vote {}/{}", votetype.to_string(), (i.saturating_sub(1)/5) + 1, num_msg))
-                    // TODO, maybe no content except on the first one?
-                .components(|mut c| {
-                    for j in 0..5 {
-                        let vali = i + j;
-                        if vali >= vlen {
-                            break;
-                        }
-                        c = c.create_action_row(|r| {
-                            r.create_button(|btn| {
-                                btn.custom_id(format!("{}{}", ID_VOTE_VAL_PREFIX, vali))
-                                    .style(ButtonStyle::Secondary)
-                                    .label(vals[vali])
-                            })
-                        });
-                    }
-                    c
+// a macro because the builder for creating and editing have the same functions, but different types
+// maybe serenity should put those in a trait
+macro_rules! setup_base_message {
+    ($m:expr, $num_votes:expr, $vtype:expr) => {
+        $m
+            .content(format!("{} Vote: {} Votes so far", $vtype, $num_votes))
+            .components(|c| {
+                c.create_action_row(|r| {
+                    r.create_button(|btn| {
+                        btn.custom_id(ID_VOTE_BTN)
+                            .style(ButtonStyle::Primary)
+                            .label("Vote!")
+                    })
                 })
-        }).await.unwrap();
-        i += 5;
+            })
+    };
+}
+
+fn create_user_message<'a, 'b, 'c>(mut c: &'b mut CreateComponents, vals: &'c Vec<&'a str>, page: usize, vote: &'c Vote) -> &'b mut CreateComponents {
+    let i = page * 4;
+    for j in 0..4 {
+        let vali = i + j;
+        if vali >= vals.len() {
+            break;
+        }
+        c = c.create_action_row(|r| {
+            r.create_button(|btn| {
+                btn.custom_id(format!("{}{}", ID_VOTE_VAL_PREFIX, vali))
+                    .style(ButtonStyle::Secondary)
+                    .label(vals[vali]) // TODO adjust label accoring to vote
+            })
+        });
+    }
+    // add a row for the movement and submit buttons
+    c.create_action_row(|r| {
+        r
+            .create_button(|btn| {
+                btn.custom_id(ID_VOTE_LEFT)
+                    .style(ButtonStyle::Secondary)
+                    .label("<")
+            })
+            .create_button(|btn| {
+                btn.custom_id(ID_VOTE_RIGHT)
+                    .style(ButtonStyle::Secondary)
+                    .label(">")
+            })
+            .create_button(|btn| {
+                btn.custom_id(ID_VOTE_SUBMIT)
+                    .style(ButtonStyle::Primary)
+                    .label("Submit")
+            })
+    })
+}
+
+async fn start_vote(ctx: &Context, cid: ChannelId, votetype: VoteType, vals: Vec<&str>, timeout: Duration) {
+    let mut vote = Vote::new(votetype);
+    let num_pages = ((vals.len() -1) / 5) + 1;
+
+    // actually let's try just having a "vote" button, so we can edit the ephemeral button to match each user
+    let basemsg = cid.send_message(ctx, |m| {
+        setup_base_message!(m, 0, votetype.to_string())
+    }).await.unwrap();
+
+    // send a ephemeral message (or multiple) to the channel for everyone, with the voting options
+    // only 5 rows per message, so we have <> btns
+    // we need to 
+
+    // let's build multiple collectors and select on them
+
+    let mut mainbtn_col = basemsg.await_component_interactions(ctx)
+        .timeout(timeout)
+        .build();
+
+    // collector for the vote buttons
+    //TODO
+    
+    // collector for the left, right, submit buttons
+    let mut nav_col = ComponentInteractionCollectorBuilder::new(ctx)
+        .timeout(timeout)
+        .channel_id(cid)
+        .filter(|i| {
+            //DEBUG
+            // filter by our ephemeral message ids? or instead mark our components with a voteid?
+            //TODO
+            // filter by component ids for these buttons
+            //TODO
+            false
+        })
+        .build();
+
+    // collector for the modals from some votes
+    //TODO
+
+    loop {
+        tokio::select! {
+            Some(interaction) = mainbtn_col.next() => {
+                println!("Got main btn interaction");
+                let mut page = 0;
+                let uid = interaction.user.id;
+                // lookup user
+                if let Some(uv) = vote.uservotes.get(&uid) {
+                    page = uv.page;
+                }
+
+                // this button brings up a new ephemeral message for voting
+                interaction.create_interaction_response(ctx, |resp| {
+                    resp.kind(InteractionResponseType::ChannelMessageWithSource).interaction_response_data(|d| {
+                        d
+                            .content(format!("Page {}/{}", page+1, num_pages))
+                            .components(|c| {
+                                create_user_message(c, &vals, page, &vote)
+                            })
+                            .ephemeral(true)
+                    })
+                }).await.unwrap();
+
+                // get the response info
+                let respmsg = interaction.get_interaction_response(ctx).await.unwrap();
+
+                if let Some(uv) = vote.uservotes.get_mut(&uid) {
+                    // delete any old ephemeral message
+                    //TODO
+                    // set the message id from the new ephemeral message
+                    uv.votemsg = respmsg.id
+                } else {
+                    // create uservote entry and record the ephemeral message id
+                    vote.uservotes.insert(uid, UserVote{
+                        votes: CastVotes::new(votetype),
+                        fullvote: false,
+                        votemsg: respmsg.id,
+                        page: 0,
+                    });
+                }
+            },
+            Some(interaction) = nav_col.next() => {
+                println!("Got nav interaction");
+                //TODO
+            },
+            else => {
+                println!("Ending collection for vote! Timed out?");
+                break
+            }
+        };
     }
 
-    // display messages with vote buttons
-    let vote = Vote::new(votetype);
-
-    // keep listening for interactions on all the button for this vote
-    // if clicked, show a modal that:
-    //    shows previous data, if any
-    //    has an input where they can give input based on voting kind
-    // at the same time we need to listen for submitions coming from those modals
-    // make sure to only collect modal interactions associated with this vote here
-    // update the user's voting data accordingly
-    // update the vote results accordingly, if the user's data is finished
-    // and display/update the ephemeral message for the user
-    // use interaction.create_followup_message / edit_followup_message? 
-    // (Will edit work if a ephemeral msg times out?)
-    //TODO
 }
 
 struct Handler;
@@ -276,7 +389,9 @@ impl EventHandler for Handler {
         }).await.unwrap();
 
         // now from the interaction above we can create the vote for everyone in the channel
+        //TODO test short timeout, see if collectors stop in select right
         start_vote(&ctx, msg.channel_id, votetype, vals, DEFAULT_TIMEOUT).await;
+
     }
 
     async fn ready(&self, _ctx: Context, _data: Ready) {
